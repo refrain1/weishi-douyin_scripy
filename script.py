edb0554b28c5e91cc9ff6ac1aa9aa74782752abe
refrain1 <32951222+refrain1@users.noreply.github.com>
2021-04-25 10:51:42 +0800

    Update script.py

--- script.py
+++ script.py
@@ -3,19 +3,35 @@ import uiautomator2 as u2
 from datetime import datetime
 import time, random, os
 
+def like(i, name):
+    i = i + 1
+    if name == 'weishi':
+        d.xpath(
+            '//*[@resource-id="com.tencent.weishi:id/rzm"]/android.view.ViewGroup[1]/android.view.ViewGroup[2]/android.widget.FrameLayout[1]').click()
+    elif name == 'douyin':
+        d(resourceId="com.ss.android.ugc.aweme.lite:id/ahr").click()
+    elif name == 'toutiao':
+        d.xpath(
+            '//*[@resource-id="com.bytedance.article.lite.plugin.huoshan:id/iv_like_video"]/android.widget.ImageView[1]').click()
+    elif name == 'kuaishou':
+        d.xpath(
+            '//*[@resource-id="com.bytedance.article.lite.plugin.huoshan:id/iv_like_video"]/android.widget.ImageView[1]').click()
+    else:
+        None
+    if name == 'weishi':
+        name = '微视'
+    elif name == 'douyin':
+        name = '抖音'
+    elif name == 'toutiao':
+        name = '今日头条'
+    elif name == 'kuaishou':
+        name = '快手'
+    else:
+        None
+    logger(f'已点赞 {name}{i}次 ！'.format(name, str(i)))
+    return i
 
-def double_click():
-    i = 0
-    i += 1
-    delay = random.uniform(0.1, 0.5)
-    time.sleep(delay)
-    randomX = str(random.randint(-15, 15) + 1015)
-    randomY = str(random.randint(-20, 20) + 1730)
-    cmd = 'adb shell input tap ' + randomX + ' ' + randomY
-    os.popen(cmd)
-    logger('已点赞 {}次 ！点击坐标为{}{}'.format(i,randomX,randomX))
-
-
+#上下滑动
 def adb_swipe_b2t(n, time_count, name):
     x1 = round(350 + random.uniform(-5, 5), 2)
     y1 = round(1580 + random.uniform(-6, 6), 2)
@@ -28,58 +44,66 @@ def adb_swipe_b2t(n, time_count, name):
     time.sleep(time_sleep)
     return time_sleep
 
-
-def adb_swipe_r2l(n,time_count,name):
-    x1 = round(912+random.uniform(-5,5),2)
-    y1 = round(990+random.uniform(-6,6),2)
-    x2 = round(128+random.uniform(-8,8),2)
-    y2 = round(800+random.uniform(-7,7),2)
-    step_ = round(200+random.randint(-9,9),2)
-    time_sleep = random.uniform(6,8)
-    os.system("adb shell input swipe {} {} {} {} {}".format(x1,y1,x2,y2,step_))
-    logger('正在看{}第{}个视频！看{}秒！总共观看{}分钟'.format(name,n,time_sleep,round(time_count/60,2)))
+#左右滑动
+def adb_swipe_r2l(n, time_count, name):
+    x1 = round(912 + random.uniform(-5, 5), 2)
+    y1 = round(990 + random.uniform(-6, 6), 2)
+    x2 = round(128 + random.uniform(-8, 8), 2)
+    y2 = round(800 + random.uniform(-7, 7), 2)
+    step_ = round(200 + random.randint(-9, 9), 2)
+    time_sleep = random.uniform(6, 8)
+    os.system("adb shell input swipe {} {} {} {} {}".format(x1, y1, x2, y2, step_))
+    logger('正在看{}第{}个视频！看{}秒！总共观看{}分钟'.format(name, n, time_sleep, round(time_count / 60, 2)))
     time.sleep(time_sleep)
     return time_sleep
 
-
-
+#打印
 def logger(msg):
     current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
     print(current_time, msg)
 
-
-def check_time(name,first_time,now_time):
+#各软件时长
+def check_time(name, first_time, now_time):
     if name == 'weishi':
         if (now_time - first_time).seconds <= 2700:
             return True
         else:
             return False
     elif name == 'douyin':
-        if (now_time-first_time).seconds <= 2700:
+        if (now_time - first_time).seconds <= 2700:
             return True
         else:
             return False
     elif name == 'toutiao':
-        if (now_time-first_time).seconds <= 2700:
+        if (now_time - first_time).seconds <= 2700:
             return True
         else:
             return False
     elif name == 'kuaishou':
         return True
-def ramdom_dpuble_click():
-    random_num = random.randint(1,20)
-    if random_num == 1 or random_num == 20:
-        double_click()
-        
+
+
+#点击喜欢
+def click_like(i, name):
+    random_num = random.randint(1, 30)
+    # random_num = 1
+    if random_num == 1 or random_num == 10:
+        try:
+            i = like(i, name)
+            return i
+        except:
+            return i
+
 
 first_time = datetime.now()
 now_time = datetime.now()
 n = 1
 time_count = 1
 d = u2.connect()
-os.system('adb shell settings put system screen_brightness {}'.format(1))
+os.system('adb shell settings put system screen_brightness {}'.format(11))
 
-#微视
+
+# 微视
 def weishi():
     d.app_stop("com.tencent.weishi")
     d.app_start("com.tencent.weishi")
@@ -87,17 +111,18 @@ def weishi():
     first_time = datetime.now()
     now_time = datetime.now()
     n = 1
+    i = 0
     time_count = 1
-
-    while check_time('weishi',first_time,now_time):
-        ramdom_dpuble_click()
+    name = 'weishi'
+    while check_time(name, first_time, now_time):
+        i = click_like(i, name)
         time_sleep = adb_swipe_b2t(n, time_count, '微视')
         now_time = datetime.now()
-        n = n+1
-        time_count = time_count +time_sleep
+        n = n + 1
+        time_count = time_count + time_sleep
 
-#抖音
 
+# 抖音
 def douyin():
     d.app_stop("com.tencent.weishi")
     d.app_stop("com.ss.android.ugc.aweme.lite")
@@ -106,18 +131,20 @@ def douyin():
     first_time = datetime.now()
     now_time = datetime.now()
     n = 1
+    i = 0
+    name = 'douyin'
     time_count = 1
-    while check_time('douyin',first_time,now_time):
-        ramdom_dpuble_click()
+    while check_time(name, first_time, now_time):
+        i = click_like(i, name)
         time_sleep = adb_swipe_b2t(n, time_count, '抖音')
         now_time = datetime.now()
-        n = n+1
-        time_count = time_count +time_sleep
+        n = n + 1
+        time_count = time_count + time_sleep
 
     d.app_stop("com.ss.android.ugc.aweme.lite")
 
 
-#今日头条
+# 今日头条
 def toutiao():
     d.app_stop("com.ss.android.ugc.aweme.lite")
     d.app_stop("com.ss.android.article.lite")
@@ -126,20 +153,23 @@ def toutiao():
     first_time = datetime.now()
     now_time = datetime.now()
     n = 1
+    i = 0
+    name = 'toutiao'
     time_count = 1
-    d.click(479.0, 2250.0)  # 点击，抢购按钮
+    d.click(479.0, 2250.0) 
     time.sleep(2)
-    d.click(99.0, 1559.0)  # 点击，抢购按钮
+    d.click(99.0, 1559.0)
     time.sleep(2)
-    d.click(99.0, 1559.0)  # 点击，抢购按钮
-    while check_time('toutiao',first_time,now_time):
+    d.click(99.0, 1559.0) 
+    while check_time(name, first_time, now_time):
         time_sleep = adb_swipe_r2l(n, time_count, '今日头条')
-        ramdom_dpuble_click()
+        i = click_like(i, name)
         now_time = datetime.now()
-        n = n+1
-        time_count = time_count +time_sleep
+        n = n + 1
+        time_count = time_count + time_sleep
 
-#快手
+
+# 快手
 def kuaishou():
     d.app_stop("com.ss.android.article.lite")
     d.app_stop("com.kuaishou.nebula")
@@ -148,15 +178,17 @@ def kuaishou():
     first_time = datetime.now()
     now_time = datetime.now()
     n = 1
+    name = 'kuaishou'
     time_count = 1
-    while check_time('kuaishou',first_time,now_time):
-        ramdom_dpuble_click()
+    while check_time(name, first_time, now_time):
+        i = click_like(i, name)
         time_sleep = adb_swipe_b2t(n, time_count, '快手')
         now_time = datetime.now()
-        n = n+1
-        time_count = time_count +time_sleep
+        n = n + 1
+        time_count = time_count + time_sleep
 
     d.app_stop("com.kuaishou.nebula")
+
 
 weishi()
 douyin()
